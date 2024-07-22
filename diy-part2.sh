f6f8a5a5d3223dd5dacbcfdb717528c8f752363c
WenJun <nczywq@qq.com>
2024-07-22 13:21:13 +0800

    添加应用过滤

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,6 +36,8 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pus
 #git clone https://github.com/VPN-V2Ray/luci-app-poweroff.git package/luci-app-poweroff
 # 添加passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+# 添加应用过滤
+git clone  https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #加入turboacc
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
 chmod -R 777 add_turboacc.sh
@@ -54,7 +56,7 @@ CONFIG_PACKAGE_luci-theme-openwrt-2020=y
 
 CONFIG_PACKAGE_luci-theme-alpha=y
 
-
+CONFIG_PACKAGE_luci-app-oaf=y
 
 
 
